245f5972e8e06002df703e05ed5dc3655063b59d
Giovanni Fungo <1936847+thefronge@users.noreply.github.com>
2020-12-12 11:56:59 -0500

    day 10 part 1 done

--- day10.py
+++ day10.py
@@ -1,1 +1,19 @@
 day10data = open("day10data.txt").read().split()
+day10data_int = [int(line) for line in day10data]
+
+# part1
+# volt_diff_3 = []
+# volt_diff_1 = []
+# previous_line = 0
+
+# for line in sorted(day10data_int):
+#     if line - previous_line == 1:
+#         volt_diff_1.append(1)
+#     if line - previous_line == 3:
+#         volt_diff_3.append(1)
+#     previous_line = line
+
+# print(len(volt_diff_1) * (len(volt_diff_3) + 1))
+
+# part2
+# fuck
